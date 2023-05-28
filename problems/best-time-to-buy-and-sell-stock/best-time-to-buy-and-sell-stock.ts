e523b23909b0ec6088fe7391f304fe4bb775cabc
Maciek Sitkowski <58401630+sitek94@users.noreply.github.com>
2023-05-28 16:02:50 +0200

    Time: 99 ms (19.94%) | Memory: 51.8 MB (68.29%) - LeetSync

--- problems/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ problems/best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -1,24 +1,16 @@
 function maxProfit(prices: number[]): number {
   let maxProfit = 0
   let minPrice = Infinity
-  let minPriceIndex = -1
-  let maxPrice = 0
-  let maxPriceIndex = -1
 
   for (let i = 0; i < prices.length; i++) {
     let currentPrice = prices[i]
 
     if (currentPrice < minPrice) {
       minPrice = currentPrice
-      minPriceIndex = i
     }
 
-    if (currentPrice > maxPrice || minPriceIndex > maxPriceIndex) {
-      maxPrice = currentPrice
-      maxPriceIndex = i
-    }
+    let currentProfit = currentPrice - minPrice
 
-    let currentProfit = maxPrice - minPrice
     if (currentProfit > maxProfit) {
       maxProfit = currentProfit
     }
