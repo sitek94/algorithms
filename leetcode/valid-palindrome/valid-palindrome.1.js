9626234b905ffe1d17463b5bbd90014c50ad3d31
Maciek Sitkowski <msitkowski94@gmail.com>
2023-07-23 15:12:37 +0200

    prepare to sync using extension

--- leetcode/valid-palindrome/valid-palindrome.1.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isPalindrome = function(s) {
-  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
-  return s === s.split('').reverse().join('')
-};
