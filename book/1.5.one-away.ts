83e2502d8dbed7e65e34e3e578a9e1c2ce8cb7d1
Maciek Sitkowski <msitkowski94@gmail.com>
2023-12-29 17:46:59 +0100

    add 1.5 description + complexity

--- book/1.5.one-away.ts
+++ book/1.5.one-away.ts
@@ -1,3 +1,18 @@
+/**
+ * Determines whether two given strings are one (or zero) edits away from each other. An edit is defined
+ * as an insertion, deletion, or replacement of a single character.
+ *
+ * The function iterates through both strings simultaneously, tracking the number of edits. If more than
+ * one edit is required, the function returns false. It efficiently handles the different lengths of strings
+ * and determines the type of edit needed accordingly.
+ *
+ * @complexity
+ * Time: O(n + m), where n is the length of the input string and m is the length of the target string.
+ * This is because in the worst case, the function may need to check each character in both strings once.
+ * In practice, the complexity is O(n), where n is the length of the longer string.
+ *
+ * Space: O(1), constant space due to a fixed number of variables used, which does not increase with input size.
+ */
 const oneAway = (input: string, target: string) => {
   const shouldInsert = input.length < target.length
   const shouldRemove = input.length > target.length
