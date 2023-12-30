b477fd5d17652b4e951afde011b14b3ba1c82869
Maciek Sitkowski <msitkowski94@gmail.com>
2023-12-30 09:14:48 +0100

    handle edge cases

--- book/1.6.string-compression.ts
+++ book/1.6.string-compression.ts
@@ -2,19 +2,22 @@
  * Perform basic string compression using the counts of repeated characters.
  *
  * Input string has only lowercase and uppercase letters a-z.
- * 
+ *
  * @complexity
  * Time: O(n), we need to go through each char of input string
  * Space: O(n), in worst case scenario where we can't compress the string
  */
 const compressString = (input: string): string => {
+  // Edge cases
+  if (input === "" || input.length === 1) {
+    return input
+  }
+
   let output = ""
   let count = 1
   let current = ""
 
   for (let char of input) {
-    console.log(char)
-
     if (char === current) {
       count++
     } else {
@@ -29,7 +32,14 @@ const compressString = (input: string): string => {
 
   output += current + String(count)
 
+  if (output.length > input.length) {
+    return input
+  }
+
   return output
 }
 
 console.assert(compressString("aabcccccaaa") === "a2b1c5a3")
+console.assert(compressString("abcdefg") === "abcdefg")
+console.assert(compressString("") === "")
+console.assert(compressString("a") === "a")
