6c6683b4e3c3057166e4f7c1490e0e5b8268bbce
Maciek Sitkowski <msitkowski94@gmail.com>
2024-01-06 10:06:12 +0100

    solve 2.3

---
+++ book/2.3.delete-middle-node.ts
@@ -0,0 +1,23 @@
+import {LinkedList, LinkedListNode, createLinkedList} from '@/utils/linked-list'
+
+const linkedList = new LinkedList(1)
+
+linkedList.appendToTail(2)
+const thirdNode = linkedList.appendToTail(3)
+linkedList.appendToTail(4)
+linkedList.appendToTail(5)
+linkedList.print()
+
+const deleteMiddleNode = (node: typeof linkedList.head) => {
+  const next = node.next
+  if (!next) {
+    throw new Error('deleteMiddleNode must be used on middle node!')
+  }
+
+  node.value = next.value
+  node.next = next.next
+}
+
+deleteMiddleNode(thirdNode)
+// Should be: 1 -> 2 -> 4 -> 5
+linkedList.print()
